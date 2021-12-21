809adf8de28ba2c3666e3547cabc1aa757eca6b8
Buff4eyes <claudemokbel@gmail.com>
2021-12-21 00:25:42 -0800

    removed env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { Product, Category, Tag, ProductTag } = require('./models')
 // import sequelize connection
 
 const app = express()
@@ -9,4 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+
+async function init() {
+  await require('./db').sync()
+  app.listen(process.env.PORT || 3000)
+}

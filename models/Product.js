9719fd8ff3d52e6d500d24acc3364aee82e53812
Buff4eyes <claudemokbel@gmail.com>
2021-12-25 20:09:55 -0800

    added .js

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@
 const { Model, DataTypes } = require('sequelize')
 const { Category } = require('./Category')
 // import our database connection from config.js
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection.js')
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -22,7 +22,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {isDecimal: true},
     },
